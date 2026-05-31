66f0f91b48bb481ec97b8b9b7b0942b57e1f8fd0
Troitskiy-code <mrcheleng87@gmail.com>
2026-05-31 12:41:37 +0300

    Update copyright year and implement addCar method

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,50 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
 #include "train.h"
+
+Train::Train() : countOp(0), first(0) {}
+
+void Train::addCar(bool light) {
+    Car* newCar = new Car;
+    newCar->light = light;
+    newCar->next = 0;
+    newCar->prev = 0;
+    if (first == 0) {
+        first = newCar;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Car* tail = first->prev;
+        newCar->next = first;
+        newCar->prev = tail;
+        tail->next = newCar;
+        first->prev = newCar;
+    }
+}
+
+int Train::getLength() {
+    first->light = true;
+
+    while (true) {
+        int steps = 0;
+        do {
+            first = first->next;
+            ++countOp;
+            ++steps;
+        } while (first->light == false);
+
+        first->light = false;
+
+        for (int i = 0; i < steps; ++i) {
+            first = first->prev;
+            ++countOp;
+        }
+
+        if (first->light == false) {
+            return steps;
+        }
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
